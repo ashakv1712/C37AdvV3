4e5379ed7b984972abe71ed18bf1cf605645e390
ashakv1712 <74943973+ashakv1712@users.noreply.github.com>
2021-11-09 12:14:53 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,30 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    
+  }
+
+  update() {
+   
+  }
+
+  start() {
+    player = new Player();
+    //playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    //create the sprites for the cars
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  //create a play function
+  
+}
